0447cbb288dfe416e56776405417c8b2fd5ed01b
Tijo Scaria <tijovscaria@gmail.com>
2026-01-30 19:14:27 +0530

    updating the dependencies

---
+++ src/common/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// src/common/GlobalUsings.cs
+global using System;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
